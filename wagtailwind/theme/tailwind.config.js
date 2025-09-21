977d3e381dce9395727a09dd9ee4d5735102287a
Abhijeet Raut <theabhijeetraut@gmail.com>
2025-09-22 02:32:14 +0530

    First Commit

---
+++ wagtailwind/theme/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    '../../templates/**/*.html',
+    '../../home/templates/**/*.html',
+    '../../search/templates/**/*.html',
+    './templates/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ wagtailwind/wagtailwind/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-yun881h&4xazko2-l&w^=blr)-((u6gq2x6_l$+#g0u2$32@!5"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
